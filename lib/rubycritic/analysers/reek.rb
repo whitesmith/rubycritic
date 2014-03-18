938102a4b51659451d3536ae1decc0d403e6018f
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-03-18 22:40:00 +0000

    Make path to a config file relative to the current file

--- lib/rubycritic/analysers/reek.rb
+++ lib/rubycritic/analysers/reek.rb
@@ -4,7 +4,7 @@ module Rubycritic
   module Analyser
 
     class Reek < ::Reek::Examiner
-      DEFAULT_CONFIG_FILES = ["lib/rubycritic/analysers/config.reek"]
+      DEFAULT_CONFIG_FILES = [File.expand_path("../config.reek", __FILE__)]
 
       def initialize(paths)
         super(Array(paths), DEFAULT_CONFIG_FILES)
