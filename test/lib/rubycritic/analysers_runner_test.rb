a2ba0097753bfde188ba91ac96052347965cb304
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-05-07 12:13:10 +0100

    Eliminate coupling between tests
    I don't think tests should depend on the same sample file, even if it's
    an empty one.

--- test/lib/rubycritic/analysers_runner_test.rb
+++ test/lib/rubycritic/analysers_runner_test.rb
@@ -4,7 +4,7 @@ require "rubycritic/analysers_runner"
 describe Rubycritic::AnalysersRunner do
   describe "#run" do
     it "returns an array containing smell adapters" do
-      smell_adapters = Rubycritic::AnalysersRunner.new("test/samples/location/file0.rb").run
+      smell_adapters = Rubycritic::AnalysersRunner.new("test/samples/analysers_runner/empty.rb").run
       smell_adapters.each { |adapter| adapter.must_respond_to(:smells) }
     end
   end
