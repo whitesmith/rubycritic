f41cafe9e7e51cc8a7765cf798d6b558cbd070fb
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-06-03 19:59:00 +0100

    Remove magic

--- lib/rubycritic/analysers_runner.rb
+++ lib/rubycritic/analysers_runner.rb
@@ -1,4 +1,3 @@
-require "rubycritic/active_support/methods"
 require "rubycritic/adapters/smell/flay"
 require "rubycritic/adapters/smell/flog"
 require "rubycritic/adapters/smell/reek"
@@ -8,9 +7,7 @@ require "rubycritic/analysers/churn"
 module Rubycritic
 
   class AnalysersRunner
-    include ActiveSupport
-
-    SMELL_ANALYSERS = ["Flay", "Flog", "Reek"]
+    SMELL_ANALYSERS = [SmellAdapter::Flay, SmellAdapter::Flog, SmellAdapter::Reek]
 
     def initialize(analysed_files, source_control_system)
       @analysed_files = analysed_files
@@ -18,8 +15,8 @@ module Rubycritic
     end
 
     def run
-      SMELL_ANALYSERS.map do |analyser_name|
-        constantize("Rubycritic::SmellAdapter::#{analyser_name}").new(@analysed_files).smells
+      SMELL_ANALYSERS.map do |analyser|
+        analyser.new(@analysed_files).smells
       end
       ComplexityAdapter::Flog.new(@analysed_files).complexity
       Analyser::Churn.new(@analysed_files, @source_control_system).churn
