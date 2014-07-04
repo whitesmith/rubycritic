dfaf673c284c67e02ce7e4b2a065cef02b7e4ce3
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-07-04 10:31:31 +0100

    Test new Configuration option to set root as an absolute path

--- test/lib/rubycritic/configuration_test.rb
+++ test/lib/rubycritic/configuration_test.rb
@@ -16,6 +16,11 @@ describe Rubycritic::Configuration do
       Rubycritic.configuration.root.must_equal File.expand_path("foo")
     end
 
+    it "can be set to an absolute path" do
+      Rubycritic.configuration.root = "/foo"
+      Rubycritic.configuration.root.must_equal "/foo"
+    end
+
     after do
       Rubycritic.configuration.root = @default
     end
