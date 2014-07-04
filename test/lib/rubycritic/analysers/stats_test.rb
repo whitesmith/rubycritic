678136c99a8051d5af7bad1b66302b076388df83
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-07-04 14:27:01 +0100

    Test Analyser::Stats

---
+++ test/lib/rubycritic/analysers/stats_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+require "rubycritic/analysers/stats"
+
+describe Rubycritic::Analyser::Stats do
+  before do
+    @analysed_files = [AnalysedFileDouble.new(:path => "test/samples/stats/example.rb")]
+  end
+
+  it "calculates the number of methods of each file and adds it to analysed_files" do
+    Rubycritic::Analyser::Stats.new(@analysed_files).stats
+    @analysed_files.each do |analysed_file|
+      analysed_file.methods_count.must_equal 1
+    end
+  end
+end
---
+++ test/samples/stats/example.rb
@@ -0,0 +1,4 @@
+class Example
+  def one_method
+  end
+end
