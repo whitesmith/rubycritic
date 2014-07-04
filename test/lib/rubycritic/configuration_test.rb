04599dfeae187f3b2512d1d38c5a0a16aea23bfc
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-07-04 10:30:47 +0100

    Refactor Configuration tests

--- test/lib/rubycritic/configuration_test.rb
+++ test/lib/rubycritic/configuration_test.rb
@@ -3,15 +3,21 @@ require "rubycritic/configuration"
 
 describe Rubycritic::Configuration do
   describe "#root" do
+    before do
+      @default = Rubycritic.configuration.root
+    end
+
     it "has a default" do
       Rubycritic.configuration.root.wont_be_empty
     end
 
-    it "can be configured" do
-      default = Rubycritic.configuration.root
+    it "can be set to a relative path" do
       Rubycritic.configuration.root = "foo"
       Rubycritic.configuration.root.must_equal File.expand_path("foo")
-      Rubycritic.configuration.root = default
+    end
+
+    after do
+      Rubycritic.configuration.root = @default
     end
   end
 end
