8c05f64b7baad4a5c88f5b16995f1003df89d3df
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-07-07 18:16:33 +0100

    Update Stats test to check that it can deal with empty Ruby files

--- test/lib/rubycritic/analysers/stats_test.rb
+++ test/lib/rubycritic/analysers/stats_test.rb
@@ -2,14 +2,19 @@ require "test_helper"
 require "rubycritic/analysers/stats"
 
 describe Rubycritic::Analyser::Stats do
-  before do
-    @analysed_files = [AnalysedFileDouble.new(:path => "test/samples/stats/example.rb")]
+  context "when a file contains Ruby code" do
+    it "calculates the number of methods of each file and adds it to analysed_files" do
+      analysed_file = AnalysedFileDouble.new(:path => "test/samples/stats/example.rb")
+      Rubycritic::Analyser::Stats.new([analysed_file]).stats
+      analysed_file.methods_count.must_equal 2
+    end
   end
 
-  it "calculates the number of methods of each file and adds it to analysed_files" do
-    Rubycritic::Analyser::Stats.new(@analysed_files).stats
-    @analysed_files.each do |analysed_file|
-      analysed_file.methods_count.must_equal 2
+  context "when a file is empty" do
+    it "calculates the number of methods as 0, adding it to analysed_files" do
+      analysed_file = AnalysedFileDouble.new(:path => "test/samples/stats/empty_example.rb")
+      Rubycritic::Analyser::Stats.new([analysed_file]).stats
+      analysed_file.methods_count.must_equal 0
     end
   end
 end
