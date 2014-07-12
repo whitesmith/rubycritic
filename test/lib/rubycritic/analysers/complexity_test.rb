50d8bc1503e2ff88648c3a5149418aa62bf1c2aa
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-07-12 18:38:59 +0100

    Inline code from `before` block

--- test/lib/rubycritic/analysers/complexity_test.rb
+++ test/lib/rubycritic/analysers/complexity_test.rb
@@ -2,13 +2,10 @@ require "analysers_test_helper"
 require "rubycritic/analysers/complexity"
 
 describe Rubycritic::Analyser::Complexity do
-  before do
-    @analysed_files = [AnalysedFileDouble.new(:path => "test/samples/flog/complex.rb")]
-    Rubycritic::Analyser::Complexity.new(@analysed_files).run
-  end
-
   it "calculates the complexity of each file and adds it to analysed_files" do
-    @analysed_files.each do |analysed_file|
+    analysed_files = [AnalysedFileDouble.new(:path => "test/samples/flog/complex.rb")]
+    Rubycritic::Analyser::Complexity.new(analysed_files).run
+    analysed_files.each do |analysed_file|
       analysed_file.complexity.must_be_kind_of Numeric
     end
   end
