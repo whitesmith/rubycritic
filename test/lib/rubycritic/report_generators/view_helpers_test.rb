742615b9a4a0c5c653fefa56edb3872fdfb3bf73
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-07-26 03:26:52 +0100

    Improve `file_path` tests
    Some somewhat duplicate tests were tweaked to test some edge cases that
    weren't being tested before.

--- test/lib/rubycritic/report_generators/view_helpers_test.rb
+++ test/lib/rubycritic/report_generators/view_helpers_test.rb
@@ -28,31 +28,6 @@ describe Rubycritic::ViewHelpers do
     end
   end
 
-  context "when the file is in a subdirectory" do
-    let(:generator) { GeneratorDouble.new("subdirectory/foo.html") }
-
-    describe "#file_path" do
-      context "when the other file is in the same directory" do
-        it "creates a relative path to a file" do
-          generator.file_path("subdirectory/bar.html").to_s.must_equal "bar.html"
-        end
-      end
-
-      context "when the other file is in the root directory" do
-        it "creates a relative path to a file" do
-          generator.file_path("bar.html").to_s.must_equal "../bar.html"
-        end
-      end
-    end
-
-    describe "#asset_path" do
-      it "creates a relative path to an asset" do
-        generator.asset_path("stylesheets/application.css").to_s
-          .must_equal "../assets/stylesheets/application.css"
-      end
-    end
-  end
-
   context "when the file is n directories deep" do
     let(:generator) { GeneratorDouble.new("lets/go/crazy/foo.html") }
 
@@ -66,6 +41,18 @@ describe Rubycritic::ViewHelpers do
       context "when the other file is in the root directory" do
         it "creates a relative path to a file" do
           generator.file_path("bar.html").to_s.must_equal "../../../bar.html"
+        end
+      end
+
+      context "when the other file has n-1 directories in common" do
+        it "creates a relative path to a file" do
+          generator.file_path("lets/go/home/bar.html").to_s.must_equal "../home/bar.html"
+        end
+      end
+
+      context "when the other file is one directory deeper" do
+        it "creates a relative path to a file" do
+          generator.file_path("lets/go/crazy/everybody/bar.html").to_s.must_equal "everybody/bar.html"
         end
       end
     end
