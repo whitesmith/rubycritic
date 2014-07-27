5324554062e276fb757b1ac2c875e7051bdfd6c5
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-07-27 16:40:20 +0100

    Improve ModulesLocator
    Now, it returns the name of the file titleized if:
    
    * The file is unparsable
    
    * The file is empty
    
    * The file has no modules
    
    * The file has modules, but not methods defined. This is done to avoid
    files that just define constants having the name of the module
    namespace.

--- lib/rubycritic/analysers/helpers/modules_locator.rb
+++ lib/rubycritic/analysers/helpers/modules_locator.rb
@@ -3,14 +3,36 @@ require "rubycritic/analysers/helpers/ast_node"
 
 module Rubycritic
 
-  module ModulesLocator
-    def self.names(path)
-      content = File.read(path)
+  class ModulesLocator
+    def initialize(analysed_module)
+      @analysed_module = analysed_module
+    end
+
+    def first_name
+      names.first
+    end
+
+    def names
+      return name_from_path if @analysed_module.methods_count == 0
+
+      content = File.read(@analysed_module.path)
       node = Parser::CurrentRuby.parse(content)
-      return [] unless node
-      node.get_module_names
+      return name_from_path unless node
+
+      names = node.get_module_names
+      if names.empty?
+        name_from_path
+      else
+        names
+      end
     rescue Parser::SyntaxError => error
-      []
+      name_from_path
+    end
+
+    private
+
+    def name_from_path
+      [@analysed_module.pathname.basename.sub_ext("").to_s.split("_").map(&:capitalize).join]
     end
   end
 
