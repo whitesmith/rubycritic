f0a427dd5cd9b0b389db6959f7495d8d878b42f7
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-10-18 19:13:06 +0100

    Rescue the invalid CLI option error

--- lib/rubycritic/cli/application.rb
+++ lib/rubycritic/cli/application.rb
@@ -5,6 +5,7 @@ module Rubycritic
   module Cli
     class Application
       STATUS_SUCCESS = 0
+      STATUS_ERROR   = 1
 
       def initialize(argv)
         @options = Options.new(argv)
@@ -14,6 +15,9 @@ module Rubycritic
         parsed_options = @options.parse
         ::Rubycritic.create(parsed_options).execute
         STATUS_SUCCESS
+      rescue OptionParser::InvalidOption => error
+        $stderr.puts "Error: #{error}"
+        STATUS_ERROR
       end
     end
   end
