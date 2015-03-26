d746d678ff6b8e36529d06d0563fc74fa32a5922
Erik Ostrom <erik@echographia.com>
2015-03-25 22:42:08 -0500

    Require Reek 2.0.2.
    This version has some small bug fixes, and some features that
    don't matter to RubyCritic. The next version will probably fix
    a crashing bug when `super` is called with parameters and a block
    (troessner/reek#404). This is the configuration change that needs
    to happen to upgrade to that version, when it's available.

--- lib/rubycritic/analysers/helpers/reek.rb
+++ lib/rubycritic/analysers/helpers/reek.rb
@@ -8,7 +8,7 @@ module Rubycritic
     DEFAULT_CONFIG_FILE = File.expand_path("../config.reek", __FILE__)
 
     def initialize(paths)
-      config = OpenStruct.new(:options => OpenStruct.new(:config_file => DEFAULT_CONFIG_FILE))
+      config = OpenStruct.new(:config_file => DEFAULT_CONFIG_FILE)
       ::Reek::Configuration::AppConfiguration.initialize_with(config)
       super(Array(paths))
     end
