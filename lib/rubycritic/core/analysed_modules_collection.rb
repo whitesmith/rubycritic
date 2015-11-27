d5f248ed3460b28d879f059bc94a75b95ba3dc16
Ancor Gonzalez Sosa <ancor@suse.de>
2015-11-27 20:36:40 +0100

    Added global score calculation

---
+++ lib/rubycritic/core/analysed_modules_collection.rb
@@ -0,0 +1,49 @@
+require "rubycritic/source_locator"
+require "rubycritic/core/analysed_module"
+
+module Rubycritic
+  class AnalysedModulesCollection
+    include Enumerable
+
+    # Limit used to prevent very bad modules to have excessive impact in the
+    # overall result. See #limited_cost_for
+    COST_LIMIT = 32
+    # Score goes from 0 (worst) to 100 (perfect)
+    MAX_SCORE = 100
+    # Projects with an average cost of 16 (or above) will score 0, since 16
+    # is where the worst possible rating (F) starts
+    ZERO_SCORE_COST = 16
+    COST_MULTIPLIER = MAX_SCORE.to_f / ZERO_SCORE_COST
+
+    def initialize(paths)
+      @modules = SourceLocator.new(paths).pathnames.map do |pathname|
+        AnalysedModule.new(:pathname => pathname)
+      end
+    end
+
+    def each(&block)
+      @modules.each(&block)
+    end
+
+    def to_json(*options)
+      @modules.to_json(*options)
+    end
+
+    def score
+      MAX_SCORE - average_limited_cost * COST_MULTIPLIER
+    rescue
+      0.0
+    end
+
+    private
+
+    def average_limited_cost
+      avg = map { |mod| limited_cost_for(mod) }.reduce(:+) / @modules.size.to_f
+      [avg, ZERO_SCORE_COST].min
+    end
+
+    def limited_cost_for(mod)
+      [mod.cost, COST_LIMIT].min
+    end
+  end
+end
---
+++ test/lib/rubycritic/core/analysed_modules_collection_test.rb
@@ -0,0 +1,109 @@
+require "test_helper"
+require "rubycritic/core/analysed_modules_collection"
+
+describe Rubycritic::AnalysedModulesCollection do
+  subject { Rubycritic::AnalysedModulesCollection.new(paths) }
+
+  describe ".new" do
+    context "with an empty path" do
+      let(:paths) { "" }
+
+      it "returns an empty collection" do
+        subject.count.must_equal 0
+      end
+    end
+
+    context "with a list of files" do
+      let(:paths) { %w(test/samples/doesnt_exist.rb test/samples/unparsable.rb test/samples/empty.rb) }
+
+      it "registers one AnalysedModule element per existent file" do
+        subject.count.must_equal 2
+        subject.all? { |a| a.is_a?(Rubycritic::AnalysedModule) }.must_equal true
+      end
+    end
+
+    context "with a directory" do
+      let(:paths) { "test/samples/" }
+
+      it "recursively registers all files" do
+        subject.count.must_equal 13
+      end
+    end
+
+    context "with redundant paths" do
+      let(:paths) { %w(test/samples/flog test/samples/flog/complex.rb) }
+
+      it "returns a redundant collection" do
+        subject.count.must_equal 3
+      end
+    end
+  end
+
+  describe "#score" do
+    context "with no modules" do
+      let(:paths) { "" }
+
+      it "returns zero" do
+        subject.score.must_equal 0.0
+      end
+    end
+
+    context "with not analysed modules" do
+      let(:paths) { "test/samples/flog" }
+
+      it "returns zero" do
+        subject.score.must_equal 0.0
+      end
+    end
+
+    context "with analysed modules" do
+      before do
+        subject.each_with_index do |mod, i|
+          mod.expects(:cost).returns costs[i]
+        end
+      end
+
+      let(:paths) { %w(test/samples/flog test/samples/flay) }
+
+      context "with perfect modules" do
+        let(:costs) { [0.0, 0.0, 0.0, 0.0] }
+
+        it "returns the maximum score" do
+          subject.score.must_equal 100.0
+        end
+      end
+
+      context "with very bad modules" do
+        let(:costs) { [16.0, 16.0, 16.0, 16.0] }
+
+        it "returns zero" do
+          subject.score.must_equal 0.0
+        end
+      end
+
+      context "with horrible modules" do
+        let(:costs) { [32.0, 32.0, 32.0, 32.0] }
+
+        it "returns zero" do
+          subject.score.must_equal 0.0
+        end
+      end
+
+      context "with mixed modules" do
+        let(:costs) { [32.0, 2.0, 0.0, 2.0] }
+
+        it "properly calculates the score" do
+          subject.score.must_equal 43.75
+        end
+      end
+
+      context "with a module above the cost limit" do
+        let(:costs) { [220.0, 2.0, 0.0, 2.0] }
+
+        it "reduces the impact in the result" do
+          subject.score.must_equal 43.75
+        end
+      end
+    end
+  end
+end
