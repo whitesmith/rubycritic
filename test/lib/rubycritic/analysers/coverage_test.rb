0e6ac4fab90690ad2a8ce61d7bb47c7f91f85969
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-10-11 23:15:45 -0400

    Make sure tests work with a sample coverage directory

--- test/lib/rubycritic/analysers/coverage_test.rb
+++ test/lib/rubycritic/analysers/coverage_test.rb
@@ -3,7 +3,7 @@
 require 'analysers_test_helper'
 require 'rubycritic/analysers/coverage'
 require 'rubycritic/source_control_systems/base'
-require 'fakefs/safe'
+require 'fakefs_helper'
 
 describe RubyCritic::Analyser::Coverage do
   describe '#run' do
@@ -12,24 +12,12 @@ describe RubyCritic::Analyser::Coverage do
     end
 
     before do
+      SimpleCov.root(PathHelper.project_path)
+      SimpleCov.coverage_dir(coverage_path)
       @analysed_module = AnalysedModuleDouble.new(path: path)
-      analysed_modules = [@analysed_module]
-      analyser = RubyCritic::Analyser::Coverage.new(analysed_modules)
+      @analysed_modules = [@analysed_module]
+      analyser = RubyCritic::Analyser::Coverage.new(@analysed_modules)
       analyser.run
-    end
-
-    around do |example|
-      FakeFS do
-        begin
-          FakeFS::FileSystem.clone(PathHelper.project_path)
-          FakeFS::FileSystem.clone(coverage_path, 'coverage')
-
-          Dir.chdir(PathHelper.project_path)
-          example.call
-        ensure
-          FakeFS::FileSystem.clear
-        end
-      end
     end
 
     context 'when analysing a file with no test coverage' do
