b890c6882cf79a76943168479dea0eb2391e05de
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-10-11 22:03:27 -0400

    Add sample directory with SimpleCov coverage date
    Make sure that `coverage_test.rb` uses FakeFS with some sample SimpleCov data

--- test/lib/rubycritic/analysers/coverage_test.rb
+++ test/lib/rubycritic/analysers/coverage_test.rb
@@ -3,14 +3,33 @@
 require 'analysers_test_helper'
 require 'rubycritic/analysers/coverage'
 require 'rubycritic/source_control_systems/base'
+require 'fakefs/safe'
 
 describe RubyCritic::Analyser::Coverage do
   describe '#run' do
+    let(:coverage_path) do
+      File.join(PathHelper.project_path, 'test', 'samples', 'coverage_sample')
+    end
+
     before do
       @analysed_module = AnalysedModuleDouble.new(path: path)
       analysed_modules = [@analysed_module]
       analyser = RubyCritic::Analyser::Coverage.new(analysed_modules)
       analyser.run
+    end
+
+    around do |example|
+      FakeFS do
+        begin
+          FakeFS::FileSystem.clone(PathHelper.project_path)
+          FakeFS::FileSystem.clone(coverage_path, 'coverage')
+
+          Dir.chdir(PathHelper.project_path)
+          example.call
+        ensure
+          FakeFS::FileSystem.clear
+        end
+      end
     end
 
     context 'when analysing a file with no test coverage' do
