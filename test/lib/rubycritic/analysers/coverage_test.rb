b281b514dadcebc1865bfb255851b7545b78b202
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-10-13 14:52:27 -0400

    Fix sample
    Make sure we don't overwrite sample

--- test/lib/rubycritic/analysers/coverage_test.rb
+++ test/lib/rubycritic/analysers/coverage_test.rb
@@ -10,14 +10,26 @@ describe RubyCritic::Analyser::Coverage do
     let(:coverage_path) do
       File.join(PathHelper.project_path, 'test', 'samples', 'coverage_sample')
     end
+    let(:resultset_file) do
+      File.join(coverage_path, '.resultset.json')
+    end
+    let(:old_content) do
+      File.read(resultset_file)
+    end
 
     before do
       SimpleCov.root(PathHelper.project_path)
+      new_content = old_content.gsub('[REPLACE_ME]', PathHelper.project_path)
+      File.write(resultset_file, new_content)
       SimpleCov.coverage_dir(coverage_path)
       @analysed_module = AnalysedModuleDouble.new(path: path)
       @analysed_modules = [@analysed_module]
       analyser = RubyCritic::Analyser::Coverage.new(@analysed_modules)
       analyser.run
+    end
+
+    after do
+      File.write(resultset_file, old_content)
     end
 
     context 'when analysing a file with no test coverage' do
