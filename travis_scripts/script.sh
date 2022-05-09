b3b6c8c4175971045fc4f2255d8394569519b66c
rishijain <jainrishi.37@gmail.com>
2022-05-09 20:45:23 +0530

    removing checks for ruby 2.4 in travis scripts

--- travis_scripts/script.sh
+++ travis_scripts/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 echo "Using Ruby v${TRAVIS_RUBY_VERSION}"
-if [ "${TRAVIS_RUBY_VERSION}" = "2.4" ] || [ "${TRAVIS_RUBY_VERSION}" = "3.0" ]; then
+if [ "${TRAVIS_RUBY_VERSION}" = "3.0" ]; then
   bundle exec rake test
 else
   bundle exec appraisal rake test
