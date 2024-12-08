876b55d24f5b41815736626a80453752fa26e95d
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-08 16:57:04 +0000

    fix: forgot some set up.

--- test/lib/rubycritic/learning/churn_gem_learning_test.rb
+++ test/lib/rubycritic/learning/churn_gem_learning_test.rb
@@ -8,7 +8,7 @@ describe "churn gem's Churn" do
       start_date: '6 years ago', # Optional: Limit analysis to a specific time range
       minimum_churn_count: 1     # Minimum number of changes to consider a file
     }
-
+    Churn::GitAnalyzer.stubs(:supported?).returns(true)
     churn_calculator = ::Churn::ChurnCalculator.new(config)
     report = churn_calculator.report(false)
     # rubycritic gemspec was changed 148 times.
