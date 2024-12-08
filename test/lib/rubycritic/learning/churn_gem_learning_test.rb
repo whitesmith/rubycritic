1ab8581bad8dbe4e5e7b7dda180a8f57fc2d0069
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-08 21:51:49 +0000

    fix: added some more set up that I missed :facepalm:.

--- test/lib/rubycritic/learning/churn_gem_learning_test.rb
+++ test/lib/rubycritic/learning/churn_gem_learning_test.rb
@@ -13,11 +13,14 @@ describe "churn gem's Churn" do
     }
     Churn::GitAnalyzer.stubs(:supported?).returns(true)
     churn_calculator = Churn::ChurnCalculator.new(config)
+    churn_calculator.stubs(:parse_log_for_changes).returns([['file.rb', 4], ['less.rb', 1]])
+    churn_calculator.stubs(:parse_log_for_revision_changes).returns(['revision'])
+
     report = churn_calculator.report(false)
 
     changes = report[:churn][:changes].first
 
-    assert_equal 'CHANGELOG.md', changes[:file_path]
+    assert_equal 'file.rb', changes[:file_path]
     assert_predicate changes[:times_changed], :positive?
   end
 end
