3c3735dfebe22bcdffab91572f66d8fcc7c1a2e1
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-08 17:12:08 +0000

    test: corrected the test as changes contains a list of files and the number of times they were changed.

--- test/lib/rubycritic/learning/churn_gem_learning_test.rb
+++ test/lib/rubycritic/learning/churn_gem_learning_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 require 'churn'
 require 'churn/calculator'
+require 'byebug'
 describe "churn gem's Churn" do
   it 'computes how many times a file was changed' do
     config = {
@@ -11,7 +12,9 @@ describe "churn gem's Churn" do
     Churn::GitAnalyzer.stubs(:supported?).returns(true)
     churn_calculator = ::Churn::ChurnCalculator.new(config)
     report = churn_calculator.report(false)
-    # rubycritic gemspec was changed 148 times.
-    assert_equal 148, report[:churn][:changes].length
+
+    changes = report[:churn][:changes].first
+    assert_equal 'CHANGELOG.md', changes[:file_path]
+    assert_equal 99, changes[:times_changed]
   end
 end
