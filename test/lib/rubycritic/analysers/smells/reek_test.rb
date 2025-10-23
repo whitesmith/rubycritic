b23c408d4d106328cfb9aa757daed23c3423bbad
Francois Buys <buys.fran@gmail.com>
2025-10-24 00:41:19 +0200

    fix: Respect exclude_paths in reek config
    Add ability to exclude files from Reek analysis using the `exclude_paths`
    option in `.reek.yml`. Updated the analyser to skip excluded files and
    added tests to verify excluded files are ignored. Also added a sample
    excluded file for testing.
    
    Fixes: https://github.com/whitesmith/rubycritic/issues/537

--- test/lib/rubycritic/analysers/smells/reek_test.rb
+++ test/lib/rubycritic/analysers/smells/reek_test.rb
@@ -6,9 +6,17 @@ require 'rubycritic/analysers/smells/reek'
 describe RubyCritic::Analyser::ReekSmells do
   context 'when analysing a smelly file' do
     before do
-      pathname = Pathname.new('test/samples/reek/smelly.rb')
-      @analysed_module = AnalysedModuleDouble.new(pathname: pathname, smells: [])
-      analysed_modules = [@analysed_module]
+      # Reset Reek configuration between tests
+      # Config is cached and can leak between tests
+      RubyCritic::Reek.instance_variable_set(:@configuration, nil)
+
+      analysed_path = Pathname.new('test/samples/reek/smelly.rb')
+      @analysed_module = AnalysedModuleDouble.new(pathname: analysed_path, smells: [])
+
+      excluded_path = Pathname.new('test/samples/reek/excluded.rb')
+      @excluded_module = AnalysedModuleDouble.new(pathname: excluded_path, smells: [])
+
+      analysed_modules = [@analysed_module, @excluded_module]
       RubyCritic::Analyser::ReekSmells.new(analysed_modules).run
     end
 
@@ -20,6 +28,10 @@ describe RubyCritic::Analyser::ReekSmells do
       messages = @analysed_module.smells.map(&:message)
 
       _(messages).wont_include "has the parameter name 'a'"
+    end
+
+    it 'ignores excluded files' do
+      _(@excluded_module.smells).must_be :empty?
     end
 
     it 'creates smells with messages' do
